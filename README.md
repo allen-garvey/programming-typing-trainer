c68bb4f4731300b27c1759ea9306e5200c08cdf9
Allen Garvey <allengarvey@hotmail.com>
2022-06-03 14:57:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Programming Typing Trainer
+
+Typing training for common programming symbols.
+
+## Dependencies
+
+* npm
+* node >= 16
+
+## Getting Started
+
+* 
+* 
+
+## License
+
+Programming Typing Trainer is released under the MIT License. See license.txt for more details.
---
+++ src/components/app.vue
@@ -0,0 +1,51 @@
+<template>
+    <div 
+        :class="$style.container"
+    >
+        <div :class="$style.targetCharacter">
+            {{ targetCharacter }}
+        </div>
+        <input type="text" @keydown="onKeyPressed" />
+    </div>
+</template>
+
+<style lang="scss" module>
+.container {
+
+}
+.targetCharacter {
+    font-size: 3.157rem;
+}
+</style>
+
+<script>
+export default {
+    components: {
+    },
+    created(){
+        this.generateTargetCharacter();
+    },
+    data(){
+        return {
+            targetCharacter: '',
+        };
+    },
+    computed: {
+    },
+    methods: {
+        generateTargetCharacter(){
+            const characters = `1234567890[]{}!@#$%^&*(){}?+_-.;\`~\\|`;
+            const charactersExceptCurrent = characters.replace(this.targetCharacter, '');
+            const randomIndex = Math.floor(Math.random() * charactersExceptCurrent.length);
+            console.log(randomIndex);
+            console.log(charactersExceptCurrent);
+            this.targetCharacter = charactersExceptCurrent[randomIndex];
+        },
+        onKeyPressed($event){
+            if($event.key === this.targetCharacter){
+                this.generateTargetCharacter();
+            }
+        },
+    }
+};
+</script>
